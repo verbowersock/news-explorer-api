087831943ff436f504a8090eb89207d4b63715c5
vermelon <bowernika@gmail.com>
2021-04-28 16:36:56 -0400

    trying to debug 500

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ app.use(() => {
 
 app.use(errors());
 app.use(errorLogger);
-
+/*
 // eslint-disable-next-line no-unused-vars
 app.use((err, req, res, next) => {
   const { statusCode = 500, message } = err;
@@ -79,3 +79,4 @@ app.use((err, req, res, next) => {
         : message,
     });
 });
+*/
